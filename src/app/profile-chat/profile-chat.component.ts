5334ea900a50526a8d4ab329a993d7f2da690495
Michał Wilewski <mikewilku1@gmail.com>
2021-12-29 12:46:32 +0100

    naprawy

---
+++ src/app/profile-chat/profile-chat.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-chat',
+  templateUrl: './profile-chat.component.html',
+  styleUrls: ['./profile-chat.component.scss']
+})
+export class ProfileChatComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
