eb5d1126a6b35a47829ee6705e351851d17faf49
Michał Wilewski <mikewilku1@gmail.com>
2021-12-29 11:34:44 +0100

    + <router-outlet></router-outlet>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { AppComponent } from './app.component';
+import {ProfileBrowsingComponent} from "./profile-browsing/profile-browsing.component";
 
 const routes: Routes = [
   { path: '', component: AppComponent, canActivate: [AuthGuard]},
-  { path: '**', redirectTo: ''}
+  { path: '**', redirectTo: ''},
+  {path:'', component: ProfileBrowsingComponent}
 ];
 
 @NgModule({
